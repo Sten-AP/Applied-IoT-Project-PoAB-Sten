8fbf72d9556a52324547eda4835ce4f0a413cdbd
Sten Hulsbergen <s122441@ap.be>
2023-10-26 11:27:23 +0200

    changed to a global file for urls

---
+++ backend/urls.py
@@ -0,0 +1,4 @@
+# -------------URLS--------------
+INFLUXDB_URL = "http://10.5.0.5:8086"
+API_URL = "http://10.5.0.4:3000"
+REACT_URLS = ["http://168.119.186.250:5000", "http://poab.iot-ap.be:5000", "http://poab.iot-ap.be"]
